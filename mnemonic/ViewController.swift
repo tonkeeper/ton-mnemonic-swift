111d2cadde445b1f119a8fb9396ec1aea705ee21
zloysergunya <zloysergunya@mail.ru>
2022-10-05 13:01:29 +0700

    Initial Commit

---
+++ mnemonic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mnemonic
+//
+//  Created by Sergey Kotov on 05.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
